f6a3834afbb631a5e616c74d281c5459023dcca2
lnb <lnbsa12345@gmail.com>
2026-05-23 22:58:46 +0530

    minor update

--- custom/scripts/wezterm.sh
+++ custom/scripts/wezterm.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 if ! command -v wezterm >/dev/null; then
-  notify-send 'Wezterm not found'
+  notify-send 'Hyprland' 'Wezterm not found'
   exit 1
 fi
 
-export VK_ICD_FILENAMES=/usr/share/vulkan/icd.d/radeon_icd.x86_64.json
+export VK_ICD_FILENAMES=/usr/share/vulkan/icd.d/radeon_icd.json
 
 exec wezterm "$@"
